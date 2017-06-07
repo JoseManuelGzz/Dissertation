d7fb74987eb0b8948c9e2254fad3da4df614ac63
José Manuel González <jmanuelgc94@gmail.com>
2017-06-07 13:55:15 +0100

    Created Opponent Model file

---
+++ gvgai-master/src/tracks/multiPlayer/advanced/sampleMCTS/OpponentModels.java
@@ -0,0 +1,24 @@
+package tracks.multiPlayer.advanced.sampleMCTS;
+
+import java.util.Random;
+
+import core.game.StateObservationMulti;
+import ontology.Types;
+import tools.ElapsedCpuTimer;
+import tools.Utils;
+
+public class OpponentModels
+{
+	public OpponentModels() {
+		
+	}
+	
+	public class Alphabeta
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
